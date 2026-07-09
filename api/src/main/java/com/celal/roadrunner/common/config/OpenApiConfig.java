947b565bc970c25cf9a5c091a54f3a3976e1af96
Celal Özdemir <celal2344@gmail.com>
2026-07-09 09:01:30 +0300

    scalar fixes

--- api/src/main/java/com/celal/roadrunner/common/config/OpenApiConfig.java
+++ api/src/main/java/com/celal/roadrunner/common/config/OpenApiConfig.java
@@ -1,18 +1,13 @@
 package com.celal.roadrunner.common.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
-
-    private static final String SECURITY_SCHEME = "basicAuth";
 
     @Bean
     public OpenAPI roadrunnerOpenApi() {
@@ -21,11 +16,6 @@ public class OpenApiConfig {
                         .title("Roadrunner API")
                         .description("REST API for the Roadrunner car rental application")
                         .version("v1")
-                        .contact(new Contact().name("Roadrunner")))
-                .components(new Components()
-                        .addSecuritySchemes(SECURITY_SCHEME, new SecurityScheme()
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("basic")))
-                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME));
+                        .contact(new Contact().name("Roadrunner")));
     }
 }
--- api/src/main/java/com/celal/roadrunner/common/config/SecurityConfig.java
+++ api/src/main/java/com/celal/roadrunner/common/config/SecurityConfig.java
@@ -2,25 +2,19 @@ package com.celal.roadrunner.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-    private static final String[] OPEN_API_PATHS = {
-            "/scalar",
-            "/scalar/**"
-    };
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         return http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(OPEN_API_PATHS).permitAll()
-                        .anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults())
+                        .anyRequest().permitAll())
                 .build();
     }
 }
